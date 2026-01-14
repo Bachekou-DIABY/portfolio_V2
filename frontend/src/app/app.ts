e2a1aa29526ee8baec52a9103ebda12ee5a86187
DIABY Bachekou <bac.diaby@gmail.com>
2026-01-14 11:11:50 +0100

    front end setup + tailwind working'

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from './components/hero-section/hero-section';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSectionComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
